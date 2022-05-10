19e1d9a005668b31e05f24dd4df67bb0c338ba99
Una Mathiesen Langeland <unla@itu.dk>
2022-05-10 10:23:53 +0200

    added spinner

--- app/src/main/java/dk/itu/moapd/scootersharing/fragments/PaymentFragment.kt
+++ app/src/main/java/dk/itu/moapd/scootersharing/fragments/PaymentFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import dk.itu.moapd.scootersharing.databinding.FragmentPaymentBinding
 
 
@@ -39,6 +40,15 @@ class PaymentFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        with(paymentBinding) {
+            payment.setOnClickListener {
+                progressBar?.visibility = View.VISIBLE
+            }
+            mobilepay.setOnClickListener {
+
+            }
+        }
 
     }
 
